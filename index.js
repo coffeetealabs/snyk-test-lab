501925a37adc137c7a1268cbc53b1e677fd7f3b9
coffeetealabs <Kyleswirl@gmail.com>
2026-03-04 11:36:30 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,19 @@ insecure(app)
 app.listen(3000, () => {
     console.log("Server running on port 3000")
 })
+
+module.exports = (app) => {
+
+    // Eval vulnerability
+    app.get("/eval", (req, res) => {
+        const userInput = req.query.code
+        const result = eval(userInput)
+        res.send(String(result))
+    })
+
+    // SQL injection example
+    function login(user, password) {
+        const query = "SELECT * FROM users WHERE user='" + user + "' AND pass='" + password + "'"
+        database.execute(query)
+    }
+}
